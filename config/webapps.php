a894c13a4ad7e6cf1a82053979f1d60f2330a82e
David Jardin <d.jardin@djumla.de>
2019-03-20 13:15:40 +0100

    adjust typo3 config

--- config/webapps.php
+++ config/webapps.php
@@ -13,7 +13,6 @@ return [
     'Typo3' => [
         'ignoredFilenames' => ['index.html', '.htaccess'],
         'ignoredFolders' => ['#^install#', '#^dev#', '#^t3lib#', '#^build#', '#.*/Private/.*#', '#.*sysext/install.*#'],
-        'webroot' => '/typo3'
     ],
     'Joomla' => [
         'ignoredFilenames' => ['index.html', 'joomla.xml', '.htaccess'],
