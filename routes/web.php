6499a50b62dbe2e38ab441b0c0139f5be6c15abc
Sascha Brendel <mail@lednerb.eu>
2019-05-14 09:40:16 +0200

    Removed closure route to enable route caching

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return 'API only! Please check the readme for the correct routes.';
-});
