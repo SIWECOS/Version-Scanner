44ae1289d94c70771bae456ab5d4440d908e9481
David Jardin <d.jardin@djumla.de>
2019-05-17 19:15:31 +0200

    Schedule update command #7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('svs:updatedatabase')->cron('0 */12 * * *');
     }
 
     /**
